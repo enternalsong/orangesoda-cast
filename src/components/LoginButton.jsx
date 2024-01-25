4d4354ccdafd099820a5884ca79c6d4f1f714311
Kspsss <cjk123cjk@gmail.com>
2024-01-25 17:42:35 +0800

    update redirect uri for netify

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -19,7 +19,8 @@ async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    // params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://steady-peony-c51943.netlify.app");
     params.append("scope", "user-read-private user-read-email user-read-playback-state playlist-read-private playlist-modify-public playlist-modify-private");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
