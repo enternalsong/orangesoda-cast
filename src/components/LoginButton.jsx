463c5e154ebee47647ba7a12e144af85729c521a
Kspsss <cjk123cjk@gmail.com>
2024-01-25 17:59:07 +0800

    Update LoginButton.jsx

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -20,7 +20,7 @@ async function redirectToAuthCodeFlow(clientId) {
     params.append("client_id", clientId);
     params.append("response_type", "code");
     // params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("redirect_uri", "alfacast.netlify.app/callback");
+    params.append("redirect_uri", "https://alfacast.netlify.app/callback");
     params.append("scope", "user-read-private user-read-email user-read-playback-state playlist-read-private playlist-modify-public playlist-modify-private");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
